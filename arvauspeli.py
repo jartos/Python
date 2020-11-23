20e1f44c88a35eb198d4eca672dda203aa0dcfab
jartos <69080331+jartos@users.noreply.github.com>
2020-11-23 12:36:01 +0200

    Update arvauspeli.py

--- arvauspeli.py
+++ arvauspeli.py
@@ -1,10 +1,10 @@
 # arvauspeli
 import random
 
-print('Moi, mikä sinun nimesi on?')
+print('Tervetuloa. Anna nimesi')
 name = input()
 
-print('Noniin ' + name + ', pelataanpa arvauspeliä. Olen arponut numeron väliltä 1-20. Arvaatko sen? Saat kuusi yritystä.')
+print('Hei ' + name + '! Pelataanpa arvauspeliä. Olen arponut numeron väliltä 1-20. Arvaatko sen? Saat kuusi yritystä.')
 secretNumber = random.randint(1, 20)
       
 for arvaukset in range (1, 7):
